ecf6099cd7aec583ed97d566057775cb2257ca56
Cory Pickrel <cpickrel4@gmail.com>
2018-02-19 13:54:08 -0600

    install browser-sync and define dev and dist deployments

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulpUglify = require('gulp-uglify');
 var gulpCssNano = require('gulp-cssnano');
 var gulpIf = require('gulp-if');
 var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync')
 
 gulp.task('useref', function() {
 	return gulp.src('./*.html')
@@ -17,4 +18,28 @@ gulp.task('images', function() {
 	return gulp.src('img/*.+(png|jpg|gif|svg)')
 		.pipe(imagemin())
 		.pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('dev', function () {
+    browserSync({
+        server: {
+            baseDir: './'
+        },
+        host: 'localhost',
+        port: 8000,
+        open: false,
+        notify: false
+    })
+});
+
+gulp.task('dist', function () {
+    browserSync({
+        server: {
+            baseDir: 'dist'
+        },
+        host: 'localhost',
+        port: 8000,
+        open: false,
+        notify: false
+    })
 });
