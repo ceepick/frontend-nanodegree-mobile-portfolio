ca2278c88491e9f1b85a5853fb4a7479a573c1db
Cory Pickrel <cpickrel4@gmail.com>
2018-02-19 13:33:51 -0600

    add imagemin gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var useref = require('gulp-useref');
 var gulpUglify = require('gulp-uglify');
 var gulpCssNano = require('gulp-cssnano');
 var gulpIf = require('gulp-if');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('useref', function() {
 	return gulp.src('./*.html')
@@ -10,4 +11,10 @@ gulp.task('useref', function() {
 		.pipe(gulpIf('*.js', gulpUglify()))
 		.pipe(gulpIf('*.css', gulpCssNano()))
 		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function() {
+	return gulp.src('img/*.+(png|jpg|gif|svg)')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img'))
 });
