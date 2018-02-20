ef0cbe33347a648cc2a2966733a444036f22f4dc
Cory Pickrel <cpickrel4@gmail.com>
2018-02-20 00:05:25 -0600

    update gulp file, update img dir paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,72 @@
 var gulp = require('gulp');
-var useref = require('gulp-useref');
+var del = require('del');
+var runSequence = require('run-sequence');
+var gulpIf = require('gulp-if');
 var gulpUglify = require('gulp-uglify');
 var gulpCssNano = require('gulp-cssnano');
-var gulpIf = require('gulp-if');
+var gulpSmoosher = require('gulp-smoosher');
+var htmlmin = require('gulp-htmlmin');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync');
 var psi = require('psi');
 
-gulp.task('useref', function() {
-	return gulp.src('./*.html')
-		.pipe(useref())
+
+// build
+
+gulp.task('clean', function() {
+	return del('dist');
+});
+
+gulp.task('buildjs', function() {
+	return gulp.src(['./js/*.js', './views/js/*.js'], {base: './'})
 		.pipe(gulpIf('*.js', gulpUglify()))
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('buildcss', function() {
+	return gulp.src(['./css/*.css', './views/css/*.css'], {base: './'})
 		.pipe(gulpIf('*.css', gulpCssNano()))
 		.pipe(gulp.dest('dist'))
 });
 
-gulp.task('images', function() {
-	return gulp.src('img/*.+(png|jpg|gif|svg)')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/img'))
+gulp.task('buildhtml', function() {
+	return gulp.src(['./*.html', './views/*.html'], {base: './'})
+		.pipe(gulpSmoosher())
+		.pipe(htmlmin({
+			collapseWhitespace: true,
+			removeComments: true,
+			minifyJS: true,
+			minifyCSS: true
+		}))
+		.pipe(gulp.dest('dist'))
 });
+
+gulp.task('buildimages', function() {
+	return gulp.src([
+		'!node_modules',
+		'!node_modules/**', 
+		'./**/img/*.+(png|jpg|gif|svg)'
+		])
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('build', function() {
+	runSequence('clean', 'buildjs', 'buildcss', 'buildhtml', 'buildimages');
+});
+
+// page speed insights
+
+gulp.task('psi', function() {
+	return psi('http://4cf4d4bd.ngrok.io/', {
+		nokey: 'true',
+		strategy: 'mobile'
+	}).then(function(data) {
+		console.log(data);
+	});
+});
+
+// deployment
 
 gulp.task('dev', function () {
 	var bs = browserSync.create();
@@ -41,14 +88,4 @@ gulp.task('dist', function () {
 	});
 	
 	bs.reload("*.html");
-});
-
-gulp.task('psi', function() {
-	return psi('http://687f6fb2.ngrok.io/', {
-		nokey: 'true',
-		strategy: 'mobile'
-	}).then(function(data) {
-		// todo: write to file
-		console.log(data);
-	});
 });
